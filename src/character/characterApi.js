cba4378803f469ccc4369855cc73b2c27c00121b
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-12-22 13:38:07 +0000

    add CryptoJS

--- src/character/characterApi.js
+++ src/character/characterApi.js
@@ -1,15 +1,12 @@
-// import CryptoJS from 'crypto-js';
-
 const PRIVATE_KEY = "a-long-hash";
 const PUBLIC_KEY = "another-long-hash";
 
 function getCharacter() {
     const ts = new Date().getTime();
-    // const hash = CryptoJS.MD5("Message");
-    // console.log(hash);
-    // const apiKey = MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
+    const CryptoJS = require("crypto-js");
+    const apiKey = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
     // // const apiKey = PRIVATE_KEY + PUBLIC_KEY;
-    // console.log(apiKey);
+    console.log("api= " + apiKey);
     return "HULK";
     // https://gateway.marvel.com:443/v1/public/characters?name=Hulkr&apikey=
 }
