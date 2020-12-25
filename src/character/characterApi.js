71b61c3a7f5b9e2a7d1ce0f20cd4d4f335236432
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-12-25 08:28:40 +0000

    tidy axios API call

--- src/character/characterApi.js
+++ src/character/characterApi.js
@@ -4,24 +4,17 @@ function getCharacter() {
 
   const ts = new Date().getTime();
   const CryptoJS = require("crypto-js");
-  const hash = CryptoJS.MD5(ts + process.env.MARVEL_API_KEY + process.env.MARVEL_PRIVATE_KEY).toString();
+  const hash = CryptoJS.MD5(ts + process.env.MARVEL_PRIVATE_KEY + process.env.MARVEL_API_KEY).toString();
   const axios = require('axios');
-  // Make a request for a user with a given ID
-  const msg = 'https://gateway.marvel.com:443/v1/public/characters?name=hulk&ts=' + ts + '&apikey=' + process.env.MARVEL_API_KEY + '&hash=' + hash;
-  console.log("msg= " + msg);
-  const output = axios.get(msg)
-    .then(function (response) {
-      // handle success
-      console.log("Success= " + response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log("ERROR! = " + error);
-    })
-    .then(function () {
-      // always executed
-    });
-  return output;
+  
+  const uri = 'https://gateway.marvel.com:443/v1/public/characters?name=hulk&ts=' + ts + '&apikey='
+    + process.env.MARVEL_API_KEY + '&hash=' + hash;
+  console.log(">>> uri= " + uri);
+  return axios.get(uri)
+    .then((response) => console.log("Success= " + JSON.stringify(response.data)))
+    .catch((error) => console.log("ERROR! = " + error));
+
+  // return output;
 }
 
 module.exports = {
