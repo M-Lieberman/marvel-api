b17500abdda1c9a1c0830afd5691e0e191f3e09c
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-12-26 12:42:50 +0000

    update working test

--- src/character/characterApi.js
+++ src/character/characterApi.js
@@ -6,15 +6,19 @@ function getCharacter() {
   const CryptoJS = require("crypto-js");
   const hash = CryptoJS.MD5(ts + process.env.MARVEL_PRIVATE_KEY + process.env.MARVEL_API_KEY).toString();
   const axios = require('axios');
-  
-  const uri = 'https://gateway.marvel.com:443/v1/public/characters?name=hulk&ts=' + ts + '&apikey='
+
+  const url = 'https://gateway.marvel.com:443/v1/public/characters?name=hulk&ts=' + ts + '&apikey='
     + process.env.MARVEL_API_KEY + '&hash=' + hash;
-  console.log(">>> uri= " + uri);
-  return axios.get(uri)
-    .then((response) => console.log("Success= " + JSON.stringify(response.data)))
+  const output = axios.get(url)
+    .then((response) => {
+      console.log("Success!");
+      console.log("\n status = " + response.status + " : " + response.statusText);
+      console.log("\n data = " + JSON.stringify(response.data));
+      return response;
+    })
     .catch((error) => console.log("ERROR! = " + error));
 
-  // return output;
+  return output;
 }
 
 module.exports = {
--- src/character/characterApi.test.js
+++ src/character/characterApi.test.js
@@ -1,17 +1,18 @@
 const {
-    getCharacter
-  } = require("./characterApi.js");
+  getCharacter
+} = require("./characterApi.js");
 // import characterApi from './characterApi';
 
 describe("test simple call to Marvel character API", () => {
   test.skip("failing test", () => {
-      return getCharacter().catch( e => expect(e).toMatch('error'));
-      });
+    return getCharacter().catch(e => expect(e).toMatch('error'));
+  });
 
-    test("returns a Marvel comic character details", () => {
-        return getCharacter().then(data => { Promise.resolve();
-          // expect(data).toBeDefined();
-          // expect(data.code).toEqual('200');
-        });
-      });
+  test("returns a Marvel comic character details", () => {
+    // expect.assertions(1);
+    return getCharacter().then(data => {
+      expect(data).toBeDefined();
+      expect(data.status).toEqual(200);
     });
+  });
+});
