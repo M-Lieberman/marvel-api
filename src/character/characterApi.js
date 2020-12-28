898676811268ad98b8f39300cd5431f6d4bf0489
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-12-28 11:04:42 +0000

    comment out log messages

--- src/character/characterApi.js
+++ src/character/characterApi.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 
-function getCharacter() {
+function getCharacter(searchCharacter) {
 
   const ts = new Date().getTime();
   const CryptoJS = require("crypto-js");
   const hash = CryptoJS.MD5(ts + process.env.MARVEL_PRIVATE_KEY + process.env.MARVEL_API_KEY).toString();
   const axios = require('axios');
 
-  const url = 'https://gateway.marvel.com:443/v1/public/characters?name=hulk&ts=' + ts + '&apikey='
+  const url = 'https://gateway.marvel.com:443/v1/public/characters?name='+ searchCharacter + '&ts=' + ts + '&apikey='
     + process.env.MARVEL_API_KEY + '&hash=' + hash;
   const output = axios.get(url)
     .then((response) => {
-      console.log("Success!");
-      console.log("\n status = " + response.status + " : " + response.statusText);
-      console.log("\n data = " + JSON.stringify(response.data));
+      // console.log("Success!");
+      // console.log("\n status = " + response.status + " : " + response.statusText);
+      // console.log("\n data = " + JSON.stringify(response.data));
       // console.log("\n results = " + JSON.stringify(response.data.data.results[0].name));
       return response;
     })
--- src/character/characterApi.test.js
+++ src/character/characterApi.test.js
@@ -3,22 +3,26 @@ const {
 } = require("./characterApi.js");
 // import characterApi from './characterApi';
 
-// TODO pass in search term to function call
-// TODO update to valid failing test
 describe("test simple call to Marvel character API", () => {
-  test.skip("failing test", () => {
+  test("expected failing test", () => {
     return getCharacter().catch(e => expect(e).toMatch('error'));
   });
 
-  test("returns a Marvel comic character details", () => {
-    // expect.assertions(1);
-    return getCharacter().then(response => {
+  test("returns Hulk comic character details", () => {
+    expect.assertions(3);
+    return getCharacter('hulk').then(response => {
       expect(response).toBeDefined();
       expect(response.status).toEqual(200);
       expect(response.data.data.results[0].name).toEqual("Hulk");
-      console.log("test= " + JSON.stringify(response.data.data.results[0].name));
-      // expect(data.results).toEqual("Hulk");
-    });
+   });
   });
+
+  test("returns Wolverine details", () => {
+    expect.assertions(2);
+    return getCharacter('wolverine').then(response => {
+      expect(response.status).toEqual(200);
+      expect(response.data.data.results[0].name).toEqual("Wolverine");
+   });
+  });  
 
 });
